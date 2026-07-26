e386c008de5b05263b12445911ac536215201a49
codi-mvp <procachprocach@gmail.com>
2026-07-26 11:07:15 +0700

    create http server

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from "http";
+
+const PORT = 8000
+const serverName = `http://localhost:${PORT}`;
+
+const server = http.createServer((req, res) => {
+    res.end("Hello World!");
+})
+server.listen(PORT, () => {
+    console.log(`Server running  ${serverName}`);
+});
